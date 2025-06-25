acbb761bd00db4efd9ffbe53b5e0ba340b88b273
papon78 <roypapon103@gmail.com>
2025-06-25 14:08:51 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Bot Token from BotFather
-define('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE');
+define('BOT_TOKEN', '8065025177:AAFm5GCJ6Jdk15wFo1itKaj1N_In-CFqnyo');
 
 // Telegram API base url
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('API_URL', 'https://api.telegram.org/bot'.8065025177:AAFm5GCJ6Jdk15wFo1itKaj1N_In-CFqnyo.'/');
 
 // Database credentials
 define('DB_HOST', 'localhost');
@@ -12,5 +12,5 @@ define('DB_PASS', '');
 define('DB_NAME', 'clickearn360');
 
 // Admin Telegram ID for admin commands
-define('ADMIN_ID', 123456789);  // Replace with your Telegram user ID
+define('ADMIN_ID', @Badolroy638);  // Replace with your Telegram user ID
 ?>
